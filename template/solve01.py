50024fcec9f0f304e54846080e2570b69c376683
SyluxDX <syluxdx@gmail.com>
2022-12-27 12:41:31 +0000

    Update template with logging template and inputt argument

--- template/solve01.py
+++ template/solve01.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 def func():
     with open(ARGS.input, "r", encoding="utf8")as ifp:
@@ -8,8 +9,14 @@ def func():
 if __name__ == "__main__":
     _parser = argparse.ArgumentParser(description="Day x Puzzle 1")
     _parser.add_argument("-i", "--input", help="Puzzle input")
+    _parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    _parser.add_argument("-vv", "--very-verbose", help="futher increase output verbosity", action="store_true")
     ARGS = _parser.parse_args()
 
+    if ARGS.verbose:
+        logging.basicConfig(level=logging.INFO, format="%(message)s")
+    if ARGS.very_verbose:
+        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
     if not ARGS.input:
         print("No input argument, use flag -i")
     else:
